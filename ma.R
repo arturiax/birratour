1d9c5e4346d374b144e86063673f96c058d0c7b2
arturiax <arturiax@hotmail.com>
2018-01-13 08:46:29 +0000

    birratour v2

--- ma.R
+++ ma.R
@@ -38,9 +38,9 @@ register_google(key = "AIzaSyBUT6aMZlrHMReT0ldvKDmfD658d5NTF8U")
 
 ggmap_credentials()
 
-lugar <- get_map("Bar Minuto, Portugalete, spain", zoom=16, source="stamen", maptype="watercolor")#, source="stamen", maptype = "watercolor")
+lugar <- get_map("Bar Minuto, Portugalete, spain", zoom=16, maptype="watercolor", source="stamen")#, source="stamen", maptype = "watercolor")
 
-
+ggmap(lugar)
 
 lista<-list("Silverstone", "Taberna Aldatza", "Café Jaque", "Bar Minuto")
 direcc<- sapply(lista, paste0, ", Portugalete, Spain")
@@ -65,14 +65,14 @@ font_add_google("Gloria Hallelujah", "gloria")
 font_add_google("Shrikhand", "shri")
 font_add_google("Bungee", "bungee")
 
-mypng <- readPNG('ppp.png')
+mypng <- readPNG("/home/art/birratour/ppp.png")
 
 
 
 showtext_auto()
 
 ani.options(interval=.15)
-saveHTML({
+saveGIF({
   
 
   q<-ggmap(lugar) +
@@ -81,7 +81,7 @@ saveHTML({
     annotation_raster(mypng, xmin = -3.0215, xmax = -3.0165, ymin=43.3165, ymax=43.3215) +
     coord_fixed(1.3) +
     theme_map()
-  for (j in 1:15) print(q)
+  for (j in 1:7) print(q)
   
   q<-ggmap(lugar) +
     annotate("rect", xmin=-3.025, xmax=-3.0132, ymin=43.316, ymax=43.325, alpha=0.6) +
@@ -89,16 +89,16 @@ saveHTML({
     coord_fixed(1.3) +
     theme_map()
   
-  for (j in 1:8) print(q)  
+  for (j in 1:6) print(q)  
   
 for (i in 1:3){
   #i<-4
   
    q<-ggmap(lugar, base_layer=ggplot(lugares[i, ], aes(lon, lat))) + 
-               geom_point(aes(color=orden), size = 7)  +
+               geom_point(aes(color=orden), size = 6)  +
                geom_text(aes(label=orden), color="white") +
  # geom_path(data=trek_df[[1]], aes(lon, lat), size=3) +
-    geom_label(aes(label=nombre), fill="tan1", size=5, color="white")  +
+    geom_label(aes(label=nombre), fill="tan1", size=5, color="white", nudge_x=ifelse(i==1,0.003,0.002))  +
       guides(fill=FALSE, color=FALSE) +
      annotation_raster(mypng, xmin=-3.0256, xmax=-3.0228, ymin=43.323, ymax=43.325)+
      geom_text_repel(family="OpenSansEmoji", label="\U1f37a") +
@@ -126,10 +126,10 @@ for (i in 1:3){
 }  
   
     q<-ggmap(lugar, base_layer=ggplot(lugares[4, ], aes(lon, lat))) + 
-    geom_point(aes(color=orden), size = 7)  +
+    geom_point(aes(color=orden), size = 6)  +
     geom_text(aes(label=orden), color="white") +
     # geom_path(data=trek_df[[1]], aes(lon, lat), size=3) +
-      geom_label(aes(label=nombre), fill="tan1", size=5, color="white")  +
+      geom_label(aes(label=nombre), fill="tan1", size=5, color="white", nudge_x=-0.002)  +
       annotation_raster(mypng, xmin=-3.0256, xmax=-3.0228, ymin=43.323, ymax=43.325)+
       geom_text_repel(family="OpenSansEmoji", label="\U1f37a") +
       guides(fill=FALSE, color=FALSE) +
@@ -148,7 +148,7 @@ for (i in 1:3){
     
     
 })  
-  
+showtext_auto(FALSE) 
 
 
 
